99a2cd99ada131c8280f56394e9b2a3dc27f923c
h <h@psdn.reboot>
2024-04-01 18:37:37 -0500

    Mejoras visulaes simplemente

--- calculadora.py
+++ calculadora.py
@@ -8,21 +8,27 @@ import os
 def main():
     #Entrar en bucle en el programa
     while True:
+        print("***" * 20)
         print("BIENVENIDO A LA CALCULADORA BINARI-HEXA")
+        print("***" * 20)
         print("Debes elegir el tipo de dato que deseas convertir")
         print("1, Para decimal")
         print("2, para binario")
         print("3, para Hexadecimal")
+        print("***" * 20)
         base = int(input())
+        print("***" * 20)
     
         if base == 1:
             while True:
                 print("A que tipo quieres convertir el decimal")
                 print("1 = Binario")
                 print("2 = Hexadecimal")
+                print("***" * 20)
                 var_Conv = int(input())
+                print("***" * 20)
                 if var_Conv == 1:
-                    num_Dec = int(input("Ingresa el numero que quieres convertir a binario\n"))
+                    num_Dec = int(input("Ingresa el numero decimal quieres convertir a binario\n"))
                     bina = bin(num_Dec)
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
@@ -30,7 +36,7 @@ def main():
                     print("==============================================")
                     break
                 elif var_Conv == 2:
-                    num_Dec = int(input("ingresa el numero que quieres convertir a hexadecimal\n"))
+                    num_Dec = int(input("ingresa el numero decimal que quieres convertir a hexadecimal\n"))
                     hexa = hex(num_Dec)
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
@@ -44,7 +50,7 @@ def main():
                 print("2 = Hexadecimal")
                 var_Conv = int(input())
                 if var_Conv == 1:
-                    num_bin = input("ingresa el numero que quieres convertir a decimal\n")
+                    num_bin = input("ingresa el numero binario que quieres convertir a decimal\n")
                     deci = int(num_bin, 2)
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
@@ -52,7 +58,7 @@ def main():
                     print("==============================================")
                     break
                 elif var_Conv == 2:
-                    num_bin = input("ingresa el numero que quieres convertir a hexadecimal\n")
+                    num_bin = input("ingresa el numero binario que quieres convertir a hexadecimal\n")
                     hexadeci = format(int(num_bin, 2),'x')
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
@@ -67,7 +73,7 @@ def main():
                 print("2 = binario")
                 var_Conv = int(input())
                 if var_Conv == 1:
-                    num_hex = input("ingresa el numero que quieres convertir a decimal\n")
+                    num_hex = input("ingresa el numero hexadecimal que quieres convertir a decimal\n")
                     deci = int(num_hex, 16)
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
@@ -75,7 +81,7 @@ def main():
                     print("==============================================")
                     break
                 elif var_Conv == 2:
-                    num_hex = input("ingresa el numero que quieres convertir a binario\n")
+                    num_hex = input("ingresa el numero hexadecimal que quieres convertir a binario\n")
                     bina = bin(int(num_bin,16))
                     os.system('cls' if os.name == 'nt' else 'clear')
                     print("==============================================")
